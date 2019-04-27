6f5ecb694c70a5f56c9b02b7f18b01929a5e6804
James Brill <contact@jamesbrill.co.uk>
2019-04-27 23:48:15 +0100

    Make post creation more dynamic

--- build.js
+++ build.js
@@ -7,48 +7,33 @@ const makeDirectory = directoryName => {
   } catch (e) {}
 };
 
-const posts = {
-  a: {
-    title: "How to win friends and influence them",
-    body: "foo bar baz"
-  },
-  b: {
-    title: "The Bible",
-    body: "foo bar baz"
-  },
-  c: {
-    title: "Alumni Warwick",
-    body: "foo bar baz"
-  }
-};
-
-const getPostLinks = postFilenames => {
-  return postFilenames.map(filename => ({
-    link: `/posts/${filename}`,
-    name: filename.split(".")[0]
+const getPostLinks = posts => {
+  return posts.map(post => ({
+    link: `/posts/${post.title}.html`,
+    name: post.title
   }));
 };
 
-const compilePosts = postFilenames => {
+const compilePosts = posts => {
   console.log("Compiling posts...");
   makeDirectory("dist/posts");
-  postFilenames.forEach(postFilename => {
-    const source = fs.readFileSync(`src/posts/${postFilename}`, "utf8");
-    const template = Handlebars.compile(source);
-    const postName = postFilename.split(".")[0];
-    const context = posts[postName];
-    const html = template(context);
-    fs.writeFile(`dist/posts/${postFilename}`, html);
+  const postSource = fs.readFileSync(`src/post.html`, "utf8");
+  const template = Handlebars.compile(postSource);
+  posts.forEach(post => {
+    const html = template(post);
+    fs.writeFile(`dist/posts/${post.title}.html`, html);
   });
 };
 
+console.log("Parsing posts...");
+const postsJson = fs.readFileSync("src/posts.json", "utf8");
+const posts = JSON.parse(postsJson);
 console.log("Compiling index...");
 const source = fs.readFileSync("src/index.html", "utf8");
 const template = Handlebars.compile(source);
-const postFilenames = fs.readdirSync("src/posts");
-const postLinks = getPostLinks(postFilenames);
+const postLinks = getPostLinks(posts);
 const context = { title: "Work In Progress", postLinks };
 const html = template(context);
 makeDirectory("dist");
 fs.writeFile("dist/index.html", html);
-compilePosts(postFilenames);
+compilePosts(posts);
